1fb2f4bc8da1b884777e7c83cc80150d175c286d
unknown <santi4586sosa@gmail.com>
2023-05-16 08:03:54 -0500

    tercer commit

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -1,29 +1,25 @@
-const validarVuelo=()=>{
+const validarVuelo = () => {
+    const departing = new Date(document.getElementById("departing").value);
+    const returning = new Date(document.getElementById("returning").value);
 
-    const departing=document.getElementById("departing").value;
-    const returning=document.getElementById("returning").value;
+    const today = new Date();
 
-    const today=new Date();
+    let mensaje = "";
 
-    let mensaje="";
-
-    if(departing < today || returning < today){
-        mensaje +='Las fechas deben ser mayores ala fecha actual';
-    }
-    else if(departing > returning){
-        mensaje +='La fecha de partida debe ser menor o igual a la de regreso';
-    }else{
-        mensaje +="Fechas agregadas exitosamente"
+    if (departing < today || returning < today) {
+        mensaje += 'Las fechas deben ser mayores a la fecha actual';
+    } else if (departing > returning) {
+        mensaje += 'La fecha de partida debe ser menor o igual a la de regreso';
     }
 
-    const alertElement=document.getElementById("mensaje");
+    const alertElement = document.getElementById("mensaje");
 
-    if(mensaje.length>0){
-        alertElement.style.display="block";
-        alertElement.innerHTML=mensaje;
-    }else{
-        alertElement.style.display="none";
+    if (mensaje.length > 0) {
+        alertElement.style.display = "block";
+        alertElement.innerHTML = mensaje;
+    } else {
+        alertElement.style.display = "none";
     }
 }
-document.querySelector("#btnRegistrarVuelo")
-.addEventListener('click',()=>validarVuelo())
+
+document.querySelector("#btnRegistrarVuelo").addEventListener('click', validarVuelo);
